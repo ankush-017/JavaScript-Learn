fe688f529c17b57271f95c4483bc5ebd574c5360
Ankush Kumar <146737320+ankush-017@users.noreply.github.com>
2025-02-25 17:25:16 +0530

    Update 01_variables.js

--- 01_variables.js
+++ 01_variables.js
@@ -1,22 +1,25 @@
+/* --------------const------------------*/
+//"const" is  Block-scoped, Cannot be Updated or Redeclared
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+accountId = 2 // not allowed
+
+/* --------------var------------------*/
+// "var" can be redeclared as well as reinitialised in its own scope , it is Function-scoped (global-variable)
 var accountPassword = "12345"
+var accountPassword = "169745" // redeclared
+accountPassword = "357896" // Reinitialises
 accountCity = "Jaipur" // By default behave a "var" keyword
+
+/* --------------let------------------*/
+// "let" Can be Updated but Not Redeclared and it is Block-scoped(local-variable)
+let accountEmail = "hitesh@google.com"
+accountEmail = "hc@hc.com" // reinitialised
 let accountState;
-
-// accountId = 2 // not allowed
-
-
-accountEmail = "hc@hc.com"
-accountPassword = "21212121"
-accountCity = "Bengaluru"
-
-console.log(accountId);
+accountState = "12365789" // reinitialised
 
 /*
 Prefer not to use var
 because of issue in block scope and functional scope
 */
-
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
