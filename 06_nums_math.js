e952204073e9c2e4df72e22c1b2a7e96b03f6319
Ankush Kumar <146737320+ankush-017@users.noreply.github.com>
2025-02-26 22:12:00 +0530

    Update 06_nums_math.js

--- 06_nums_math.js
+++ 06_nums_math.js
@@ -9,8 +9,9 @@
 
 //  const otherNumber = 365.25364;
 
-//  console.log(otherNumber.toPrecision(5)); // 365.25
-//  console.log(typeof(otherNumber.toFixed(2))); //  string
+ const otherNumber = 365.25358;
+//  console.log(otherNumber.toPrecision(5)); // 365.25 (means upto 5 digit came)
+  console.log(typeof(otherNumber.toFixed(2))); //  string
 
 //  const hundreds = 1234567885;
 //  console.log(hundreds.toLocaleString('en-IN')); //1,23,45,67,885
@@ -18,15 +19,15 @@
  // +++++++++  Maths ++++++++//
 
 // console.log(Math);
+console.log(typeof Math); // object
 // console.log(Math.abs(-1225)); // 1225
 // console.log(Math.round(4.5)); // 5
 // console.log(Math.pow(2,5));  // 32
 
-console.log(Math.random()); // always come diffrent vale between 0 to 1;
+console.log(Math.random()); // always come diffrent value between 0 to 1(exclude);
 console.log(Math.floor((Math.random()*6)+1)); // value come between 1 to 6
-
 
 const min = 10;
 const max = 100;
 
-console.log(Math.floor(Math.random()*(max-min+1))+min);
+console.log(Math.floor(Math.random()*(max-min+1))+min); // any number between (10 to 100) cane randomly
